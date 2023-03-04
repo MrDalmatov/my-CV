235c9d20a0c0054d42c1ae026fa644b78bcc6b43
Rodion <mrDalmatov@MacBook-Pro-Rodion.local>
2023-03-04 17:06:03 +0700

    feat: add dark theme

--- js/script.js
+++ js/script.js
@@ -1,28 +1,66 @@
+// Получаем ссылки на элементы страницы
 const headerNav = document.querySelector('.header-nav');
 const burgerBtn = document.querySelector('.burger-icon');
 const closeBtn = document.querySelector('.close-icon');
 const menuLinks = document.querySelectorAll('.header-link');
+const switchToggle = document.querySelector('.switch input[type="checkbox"]');
+const body = document.body;
+const upBtn = document.querySelector('.up-btn');
 
+// Функция для открытия меню
 function openHeaderNav() {
   headerNav.classList.add('open');
-  document.body.style.overflow = 'hidden';
+  body.style.overflow = 'hidden'; // устанавливаем стиль для body
 }
 
+// Функция для закрытия меню
 function closeHeaderNav() {
   headerNav.classList.remove('open');
-  document.body.style.overflow = 'auto';
+  body.style.overflow = ''; // удаляем стиль для body
 }
 
+// Функция для проверки мобильного устройства
 function isMobile() {
-  return window.innerWidth < 768;
+  return window.innerWidth < 992;
 }
 
-menuLinks.forEach(el => el.addEventListener('click', (e) => {
+// Добавляем обработчик клика для каждой ссылки в меню
+menuLinks.forEach(link => link.addEventListener('click', () => {
   if (isMobile()) {
-    closeHeaderNav();
+    closeHeaderNav(); // закрываем меню на мобильном устройстве при клике на ссылку
   }
 }));
 
+// Функция для переключения темы страницы
+function toggleDarkTheme() {
+  const isDark = switchToggle.checked;
+  body.classList.toggle('dark', isDark); // добавляем/удаляем класс 'dark' в зависимости от значения переключателя
+  localStorage.setItem('darkTheme', isDark ? 'on' : 'off'); // сохраняем состояние темной темы в Local Storage
+}
+
+// Добавляем обработчик изменения состояния переключателя темы
+switchToggle.addEventListener('change', toggleDarkTheme);
+
+// Устанавливаем тему страницы при загрузке
+window.addEventListener('load', () => {
+  const isDark = localStorage.getItem('darkTheme') === 'on';
+  body.classList.toggle('dark', isDark); // добавляем/удаляем класс 'dark' в зависимости от значения из Local Storage
+  switchToggle.checked = isDark; // устанавливаем состояние переключателя
+});
+
+// Функция для прокрутки страницы вверх
+function scrollTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Добавляем обработчик клика для кнопки прокрутки страницы вверх
+upBtn.addEventListener('click', scrollTop);
+
+// Добавляем обработчики клика для кнопок открытия/закрытия меню
 burgerBtn.addEventListener('click', openHeaderNav);
 closeBtn.addEventListener('click', closeHeaderNav);
+
 
